05b0317ea453c416f9f56d71952e537b401750d1
GammasGame <domhough@hotmail.co.uk>
2023-07-05 18:55:57 +0100

    deleted files / changed path

--- Custom.py
+++
@@ -1,8 +0,0 @@
-class Custom:
-  def __init__(self, position, rotation, scale, mesh, collider):
-    self.position = position
-    self.rotation = rotation
-    self.scale = scale
-    self.mesh = mesh
-    self.collider = collider
-    
