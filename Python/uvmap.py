8461c28a0eb24f83ef1c1b44f68434b7c05637fc
GammasGame <domhough@hotmail.co.uk>
2023-07-22 04:38:43 +0100

    creating way to unwrap shapes into uv map

---
+++ Python/uvmap.py
@@ -0,0 +1,15 @@
+from Tree import TreeNode
+
+def uv_unwrap(object):
+  selected = [object.face[0]]
+  node = TreeNode(object.face[0])
+  # start at face
+  while True:
+    # create a list of connecting face that haven't been selected before
+    # if connecting faces:
+      # select a random face
+    # else
+    # go back to previous face
+    # if at original face and no connecting faces
+      # break
+
