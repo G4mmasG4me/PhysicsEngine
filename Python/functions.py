5f4f0f657290f220ac7b31ab7a832246dddc8862
GammasGame <domhough@hotmail.co.uk>
2023-07-25 12:01:07 +0100

    added error checking in clamp function

--- Python/functions.py
+++ Python/functions.py
@@ -1,2 +1,10 @@
+import math
+
 def clamp(n, minn, maxn):
-  return max(min(maxn, n), minn)
+  try:
+    return max(min(maxn, n), minn)
+  except Exception as e:
+    print(f'Error: {e}')
+    print(f'N: {n}')
+    print(f'Is NaN: {math.isnan(n)}')
+    quit()
